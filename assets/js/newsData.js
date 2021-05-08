341541db75251dabccbaf3071c71f6b34debf529
EvgenStr <juve2910@gmail.com>
2021-05-08 23:13:38 +0300

    add user-select:none for logo

--- assets/js/newsData.js
+++ assets/js/newsData.js
@@ -1,14 +1,14 @@
 let newsData = [{
   title: "News Title",
-  date: "Date",
+  date: "05-12-2020",
   content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ducimus quis reprehenderit iure necessitatibus,  ipsum eos ullam quo quam a aut incidunt cum vitae, dolor pariatur libero repellat. Minima, hic.",
 }, {
   title: "News Title",
-  date: "Date",
+  date: "17-01-2021",
   content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ducimus quis reprehenderit iure necessitatibus,  ipsum eos ullam quo quam a aut incidunt cum vitae, dolor pariatur libero repellat. Minima, hic.",
 }, {
   title: "News Title",
-  date: "Date",
+  date: "05-04-2021",
   content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ducimus quis reprehenderit iure necessitatibus,  ipsum eos ullam quo quam a aut incidunt cum vitae, dolor pariatur libero repellat. Minima, hic.",
 }];
 
